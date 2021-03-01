2fc5750b063e72f41d35e1e92aee2808634883cd
Alvin Novian <alvinnovian883@gmail.com>
2021-03-01 11:34:47 +0700

    v0.0.8

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,38 @@
 const prompt = require("./prompt");
-const execShell = require("exec-sh").promise;
+const execShell = require("exec-sh");
+const chalk = require("chalk");
 
 const runCreateReactApp = async () => {
   const options = await prompt();
 
-  await execShell(`npx create-react-app ${options.folderName}`).catch((err) =>
-    console.error(err)
+  execShell(
+    `npx create-react-app ${options.folderName}`,
+    function (err, stdout) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(
+          "%s Finish installing Create React App!",
+          chalk.green.bold("DONE"),
+        );
+        console.log("%s Wait there's more!", chalk.yellow.bold("PROCESSING"));
+
+        execShell(
+          [`cd ${options.folderName}`, `npm install -D sass @craco/craco`],
+          function (err, stdout) {
+            if (err) {
+              console.error(err);
+            }
+          },
+        );
+      }
+      cd;
+    },
   );
+
+  //   await execShell(`npm install -D sass @craco/craco`).catch((err) => {
+  //     console.error(err);
+  //   });
 };
 
 export const run = () => runCreateReactApp();
---
+++ src/template/import-aliases.js
@@ -0,0 +1,10 @@
+const aliases = {
+  "@components": "/src/components",
+  "@icons": "/src/components/icons",
+  "@contexts": "/src/contexts",
+  "@hooks": "/src/hooks",
+  "@utils": "/src/utils",
+  "@styles": "/src/styles",
+};
+
+module.exports = aliases;
